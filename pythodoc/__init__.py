8afae9e4855f15795e0e9039cb6ef266b175cef0
thunder-007 <v.harshavardhan3@gmail.com>
2023-02-24 18:33:19 +0530

    model functional and endpoint working

--- pythodoc/__init__.py
+++ pythodoc/__init__.py
@@ -4,10 +4,23 @@ from PIL import Image
 import onnxruntime
 import torchvision
 from .model_attributes import train_classes
-
+import numpy as np
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     ort_session = onnxruntime.InferenceSession("models/plant-disease.onnx")
-    logging.info('Python HTTP trigger function processed a request.')
-    name = req.params.get('name')
-    return func.HttpResponse("onnx runtime and torch vision good to go")
+    transformer = torchvision.transforms.ToTensor()
+    crop_image = req.files.get('crop_image')
+    logging.info(crop_image)
+    if not crop_image:
+        return func.HttpResponse("Check image and reupload", status_code=400)
+    pillow_obj = Image.open(crop_image)
+    resized_image = pillow_obj.resize((256, 256), Image.LANCZOS)
+    image_tensor = transformer(resized_image)
+    image_np = image_tensor.unsqueeze(0).cpu().detach().numpy()
+    ort_inputs = {ort_session.get_inputs()[0].name: image_np}
+    ort_outs = ort_session.run(None, ort_inputs)
+    onnx_result = train_classes[int(np.argmax(ort_outs[0], axis=1))]
+    disease = onnx_result
+    if disease:
+        return func.HttpResponse(disease)
+    return func.HttpResponse("Intenal Server Error", status_code=500)
