8b713d1a4a4092bba5450d5b89a4c5d1c60d29f3
thunder-007 <v.harshavardhan3@gmail.com>
2023-02-24 18:20:22 +0530

    torchvision added to requirements.txt

--- pythodoc/__init__.py
+++ pythodoc/__init__.py
@@ -2,23 +2,12 @@ import logging
 import azure.functions as func
 from PIL import Image
 import onnxruntime
+import torchvision
+from .model_attributes import train_classes
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     ort_session = onnxruntime.InferenceSession("models/plant-disease.onnx")
     logging.info('Python HTTP trigger function processed a request.')
     name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    return func.HttpResponse("onnx runtime and torch vision good to go")
